b0d5b6fd684d31b03f576209f35c3e6277732b93
Tomáš Linhart <pasmen@gmail.com>
2021-01-20 08:37:08 +0100

    Refactor to provide better output

--- github_backup.sh
+++ github_backup.sh
@@ -8,21 +8,45 @@ echo "Working directory ${DIR}"
 find ${DIR} -mindepth 1 -maxdepth 1 -type d | \
     xargs -I % sh -c "echo 'Deleting directory %'; rm -rf %"
 
-cd ${DIR} && \
-    curl -s -H "Authorization: token ${TOKEN}" \
-               'https://api.github.com/user/repos?per_page=100' | \
-    jq -r -c '.[] | select(.fork == false)' | \
-    while read repo; do
-        full_name=$(echo "${repo}" | jq -r '.full_name')
-        clone_url=$(echo "${repo}" | \
-                    jq -r '.clone_url' | \
-                    sed -E "s|(https?://)(.*)|\1${TOKEN}@\2|")
-        has_wiki=$(echo "${repo}" | jq -r '.has_wiki')
+REPOS=$(curl -s -H "Authorization: token ${TOKEN}" \
+            'https://api.github.com/user/repos?per_page=100' | \
+        jq -r -c '.[] | select(.fork == false)')
 
-        git clone ${clone_url} ${full_name}
-        if [ "${has_wiki}" == "true" ]; then
-            wiki_clone_url=$(echo "${clone_url}" | \
-                             sed -E "s/(.*)(\.git|$)/\1.wiki\2/")
-            git clone ${wiki_clone_url} "${full_name}.wiki"
+ok_count=0
+failed_count=0
+
+cd ${DIR}
+while read repo; do
+    full_name=$(echo "${repo}" | jq -r '.full_name')
+    clone_url=$(echo "${repo}" | \
+                jq -r '.clone_url' | \
+                sed -E "s|(https?://)(.*)|\1${TOKEN}@\2|")
+    has_wiki=$(echo "${repo}" | jq -r '.has_wiki')
+
+    echo -n "Cloning ${full_name} ... "
+    git clone ${clone_url} ${full_name} >/dev/null 2>&1
+    if [ $? == 0 ]; then
+        echo "OK"
+        ok_count=$((ok_count+1))
+    else
+        echo "Failed"
+        failed_count=$((failed_count+1))
+    fi
+
+    if [ "${has_wiki}" == "true" ]; then
+        wiki_clone_url=$(echo "${clone_url}" | \
+                         sed -E "s/(.*)(\.git|$)/\1.wiki\2/")
+
+        echo -n "Cloning ${full_name}.wiki ... "
+        git clone ${wiki_clone_url} "${full_name}.wiki" >/dev/null 2>&1
+        if [ $? == 0 ]; then
+            echo "OK"
+            ok_count=$((ok_count+1))
+        else
+            echo "Failed"
+            failed_count=$((failed_count+1))
         fi
-    done
+    fi
+done <<< ${REPOS}
+
+echo "OK: ${ok_count} | Failed: ${failed_count}"
